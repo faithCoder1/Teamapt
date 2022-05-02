12e21449b3f6663f09f0b6ea02bc48a609a360fa
Oluyemisi Yesufu <ofyesufu@gmail.com>
2022-05-02 09:34:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ st.write(pd.DataFrame({
 fig1, ax1 = plt.subplots()
 explode = (0.1, 0, 0, 0,0)
 ax1.pie(servce_summ, explode=explode, labels=['MTN', 'Airtel', '9mobile', 'Globacom', 'MTEL'],
-        shadow=False, startangle=90,colors=['yellow','red','#99ff99','#green','black'])
+        shadow=False, startangle=90,colors=['yellow','red','#99ff99','green','black'])
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
 st.pyplot(fig1)
